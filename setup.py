5c01a501a7eac6a9f040251c4cb9d06a91718868
Andrii Kurinnyi <prigun@gmail.com>
2009-06-25 07:41:42 +0000

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-authorizenet',
+      version='0.1alpha',
+      description='Django and Authorize.NET payment gateway integration',
+      author='Andrii Kurinnyi',
+      author_email='andrew@zen4ever.com',
+      url='http://github.com/zen4ever/django-authorizenet/tree/master',
+      packages=['authorizenet'],
+)
+
