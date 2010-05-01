6c0f11f0f99a1b1d46194ad5cf3672530715afff
Andrii Kurinnyi <andrew@marpasoft.com>
2010-04-30 20:35:10 -0700

    Added example views for capturing auth_only transactions

--- sample_project/samplestore/views.py
+++ sample_project/samplestore/views.py
@@ -4,9 +4,10 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import Site
+from authorizenet.models import Response
 from authorizenet.forms import SIMPaymentForm, SIMBillingForm
 from authorizenet import AUTHNET_POST_URL, AUTHNET_TEST_POST_URL
-from authorizenet.utils import get_fingerprint
+from authorizenet.utils import get_fingerprint, capture_transaction
 from samplestore.models import Invoice, Item, Address
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, Http404
@@ -107,3 +108,19 @@ def make_direct_payment(request, invoice_id, auth_only=False):
     pp = AIMPayment(extra_data=extra_data, context={'item':invoice.item}, initial_data=initial_data)
     return pp(request)
 
+@login_required
+def capture_index(request):
+    responses = Response.objects.filter(type='auth_only')
+    if request.user.is_staff:
+        return render_to_response('samplestore/capture_index.html', {'responses':responses}, context_instance=RequestContext(request))
+    raise Http404
+
+@login_required
+def capture(request, id):
+    response = get_object_or_404(Response, id=id, type='auth_only')
+    if Response.objects.filter(trans_id=response.trans_id, type='prior_auth_capture').count()>0:
+        raise Http404
+    if request.user.is_staff:
+        new_response = capture_transaction(response)
+        return render_to_response('samplestore/capture.html', {'response': response, 'new_response': new_response}, context_instance=RequestContext(request))
+    raise Http404
