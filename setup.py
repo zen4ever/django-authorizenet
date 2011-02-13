059524e39db4daa1f3bb9925cc73e8929edaf3ad
Andrii Kurinnyi <andrew@marpasoft.com>
2011-02-12 22:57:51 -0800

    Updated setup.py, added MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
 setup(name='django-authorizenet',
-      version='0.1.2',
+      version='1.0',
       description='Django and Authorize.NET payment gateway integration',
       author='Andrii Kurinnyi',
       author_email='andrew@zen4ever.com',
       url='http://github.com/zen4ever/django-authorizenet/tree/master',
-      packages=['authorizenet'],
+      packages=['authorizenet', 'authorizenet.migrations'],
+      keywords=['django', 'Authorize.NET', 'payment'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Framework :: Django',
+          'Topic :: Office/Business :: Financial',
+      ],
+      lond_description=open(
+          os.path.join(os.path.dirname(__file__), 'README.rst'),
+      ).read().strip(),
 )
