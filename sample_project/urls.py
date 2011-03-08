42669d9e81b887f9b44b900dcd57bcf69fc435dc
Andrii Kurinnyi <andrew@marpasoft.com>
2011-03-07 22:11:11 -0800

    Updated the way admin urls included to Django 1.2 style

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
 )
