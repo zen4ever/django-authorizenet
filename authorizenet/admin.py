e35c02b5260ac4fa1f725b4da6f3394437d2184e
Trey Hunner <treyhunner@gmail.com>
2011-05-06 10:42:57 -0700

    Link to Response from CIM response change form

--- authorizenet/admin.py
+++ authorizenet/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 from authorizenet.models import Response, CIMResponse
 
 
@@ -15,6 +17,14 @@ admin.site.register(Response, ResponseAdmin)
 class CIMResponseAdmin(admin.ModelAdmin):
     list_display = ['result_code',
                     'result']
-    readonly_fields = ('created',)
+    readonly_fields = ('created', 'response_link')
+    exclude = ['transaction_response',]
+
+    def response_link(self, obj):
+        change_url = reverse('admin:authorizenet_response_change',
+                args=(obj.transaction_response.id,))
+        return mark_safe('<a href="%s">%s</a>' % (change_url,
+            obj.transaction_response))
+    response_link.short_description = 'transaction response'
 
 admin.site.register(CIMResponse, CIMResponseAdmin)
