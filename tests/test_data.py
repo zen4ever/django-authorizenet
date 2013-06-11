02fbc2c8685e23cc3737b8b1809d1dae786ea7fc
Trey Hunner <trey@treyhunner.com>
2013-06-11 16:49:21 -0700

    Add missing test_data module

---
+++ tests/test_data.py
@@ -0,0 +1,63 @@
+create_profile_success = {
+    'createCustomerProfileRequest': {
+        'xmlns': 'AnetApi/xml/v1/schema/AnetApiSchema.xsd',
+        'profile': {
+            'merchantCustomerId': '42',
+            'paymentProfiles': {
+                'billTo': {
+                    'firstName': 'Danielle',
+                    'lastName': 'Thompson',
+                    'company': '',
+                    'address': '101 Broadway Avenue',
+                    'city': 'San Diego',
+                    'state': 'CA',
+                    'zip': '92101',
+                    'country': 'US'
+                },
+                'payment': {
+                    'creditCard': {
+                        'cardCode': '123',
+                        'cardNumber': "5586086832001747",
+                        'expirationDate': '2020-05'
+                    }
+                }
+            }
+        },
+        'merchantAuthentication': {
+            'transactionKey': 'key',
+            'name': 'loginid'
+        },
+    }
+}
+
+
+update_profile_success = {
+    'updateCustomerPaymentProfileRequest': {
+        'xmlns': 'AnetApi/xml/v1/schema/AnetApiSchema.xsd',
+        'customerProfileId': '6666',
+        'paymentProfile': {
+            'customerPaymentProfileId': '7777',
+            'billTo': {
+                'firstName': 'Danielle',
+                'lastName': 'Thompson',
+                'company': '',
+                'address': '101 Broadway Avenue',
+                'city': 'San Diego',
+                'state': 'CA',
+                'zip': '92101',
+                'country': 'US'
+            },
+            'payment': {
+                'creditCard': {
+                    'cardCode': '123',
+                    'cardNumber': "5586086832001747",
+                    'expirationDate': '2020-05'
+                }
+            }
+        },
+        'merchantAuthentication': {
+            'transactionKey': 'key',
+            'name': 'loginid'
+        },
+    }
+}
