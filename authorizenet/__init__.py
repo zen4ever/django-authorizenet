34e2dd8486562a192f6509385690caaa3b8e61f9
Preston <preston@velocitywebworks.com>
2016-05-10 16:25:58 -0600

    updated to use Akamai transaction urls

--- authorizenet/__init__.py
+++ authorizenet/__init__.py
@@ -1,4 +1,5 @@
-AUTHNET_POST_URL = "https://secure.authorize.net/gateway/transact.dll"
+AUTHNET_POST_URL = "https://secure2.authorize.net/gateway/transact.dll"
 AUTHNET_TEST_POST_URL = "https://test.authorize.net/gateway/transact.dll"
 AUTHNET_TEST_CIM_URL = "https://apitest.authorize.net/xml/v1/request.api"
-AUTHNET_CIM_URL = "https://api.authorize.net/xml/v1/request.api"
+AUTHNET_CIM_URL = "https://api2.authorize.net/xml/v1/request.api"
+
