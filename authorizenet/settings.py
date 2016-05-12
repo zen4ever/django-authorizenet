b4a0b322eab8e813ee01c8ff96285bcf7c0f3600
Preston <preston@velocitywebworks.com>
2016-05-12 13:01:14 -0600

    allowing urls to be overridden in settings.py of site

--- authorizenet/settings.py
+++ authorizenet/settings.py
@@ -1,4 +1,21 @@
-AUTHNET_POST_URL = "https://secure2.authorize.net/gateway/transact.dll"
-AUTHNET_TEST_POST_URL = "https://test.authorize.net/gateway/transact.dll"
-AUTHNET_TEST_CIM_URL = "https://apitest.authorize.net/xml/v1/request.api"
-AUTHNET_CIM_URL = "https://api2.authorize.net/xml/v1/request.api"
+from django.conf import settings
+
+try:
+    AUTHNET_POST_URL = settings.AUTHNET_POST_URL
+except AttributeError:
+    AUTHNET_POST_URL = "https://secure2.authorize.net/gateway/transact.dll"
+
+try:
+    AUTHNET_TEST_POST_URL = settings.AUTHNET_TEST_POST_URL
+except AttributeError:
+    AUTHNET_TEST_POST_URL = "https://test.authorize.net/gateway/transact.dll"
+
+try:
+    AUTHNET_TEST_CIM_URL = settings.AUTHNET_TEST_CIM_URL
+except AttributeError:
+    AUTHNET_TEST_CIM_URL = "https://apitest.authorize.net/xml/v1/request.api"
+
+try:
+    AUTHNET_CIM_URL = settings.AUTHNET_CIM_URL
+except AttributeError:
+    AUTHNET_CIM_URL = "https://api2.authorize.net/xml/v1/request.api"
